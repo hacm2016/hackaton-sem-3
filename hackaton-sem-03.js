fad74c7b9c8f9ac507897556f90e402a22439e32
hacm2016 <hacm2016@gmail.com>
2020-04-18 12:33:32 -0500

    cambio

--- hackaton-sem-03.js
+++ hackaton-sem-03.js
@@ -1,65 +1,61 @@
 
-let arreglo= ['1', '2', '3', '4', '5'];
+let arreglo = ['1', '2', '3', '4', '5'];
 //ejercicio 01
 
-function miforeach(arreglo, callback){
-    let mynuevoarray=[];  
-    for (var i = 0; i < arreglo.length; i++) {
-      let dato = arreglo[i];    
-      callback(dato, i, arreglo);  
-      console.log(callback(dato, i, arreglo));
-     mynuevoarray.push(callback(dato, i, arreglo));
-    }
-    return mynuevoarray;
+function miforeach(arreglo, callback) {
+  let mynuevoarray = [];
+  for (var i = 0; i < arreglo.length; i++) {
+    let dato = arreglo[i];
+    callback(dato, i, arreglo);
+    console.log(callback(dato, i, arreglo));
+    mynuevoarray.push(callback(dato, i, arreglo));
   }
-let recorriendo=miforeach(arreglo,function callback(element)
-{return element;}
+  return mynuevoarray;
+}
+let recorriendo = miforeach(arreglo, function callback(element) { return element; }
 );
-console.log("Ejercicio 01",recorriendo);
+console.log("Ejercicio 01", recorriendo);
 
 //ejercicio 02
-let arreglonum= [10, 20, 30, 40, 50];
+let arreglonum = [10, 20, 30, 40, 50];
 
 
 function miMap(arr, fn) {
-    let nuevoArray = [];
-    for (var i = 0; i < arr.length; i++) {
-      nuevoArray.push(fn(arr[i]));
-    }
-    return nuevoArray;
+  let nuevoArray = [];
+  for (var i = 0; i < arr.length; i++) {
+    nuevoArray.push(fn(arr[i]));
   }
-  
-  let recorriendomap=miMap(arreglonum,function callback(element)
-  {return element *10;}
-  );
-  console.log("Ejercicio 02",recorriendomap);
-  
+  return nuevoArray;
+}
+
+let recorriendomap = miMap(arreglonum, function callback(element) { return element * 10; }
+);
+console.log("Ejercicio 02", recorriendomap);
+
 //ejercicio 03
-let arreglonumfil= [4, 8, 12, 13, 15,16,19,22];
+let arreglonumfil = [4, 8, 12, 13, 15, 16, 19, 22];
 
 
 function miFilter(arr, fn) {
-    let nuevoArrayFilter = [];
+  let nuevoArrayFilter = [];
 
-   arr.forEach(function (element,index,arr){
-     if(fn(element,index,arr))
-     {
-         nuevoArrayFilter.push(element);
-     }
-   }
+  arr.forEach(function (element, index, arr) {
+    if (fn(element, index, arr)) {
+      nuevoArrayFilter.push(element);
+    }
+  }
 
 
-   );
-    return nuevoArrayFilter;
-  } 
-  
-  let recorriendofilter=miFilter(arreglonumfil,function(element)
-  {
-      return element>=15;
-}
   );
-  console.log("Ejercicio 03-filter",recorriendofilter); 
-  
+  return nuevoArrayFilter;
+}
+
+let recorriendofilter = miFilter(arreglonumfil, function (element) {
+  return element >= 15;
+}
+);
+console.log("Ejercicios 03-filter", recorriendofilter); 
+
 
 
 
